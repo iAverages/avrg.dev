9c9627c1f433f2bc22ca2b4d63c23fcaaa795c46
iAverages <31937542+iAverages@users.noreply.github.com>
2023-05-14 17:49:04 +0100

    Retry fetching image on request
    Since I return the url early, sometimes when I paste it a 404 get cached, this hopefully will work around that

--- apps/worker/src/controllers/imageController.ts
+++ apps/worker/src/controllers/imageController.ts
@@ -3,15 +3,39 @@ import { IRequest } from "itty-router";
 import { Env } from "..";
 import { formatHeaders } from "../helpers/headers";
 
+const retry = <T>(tries: number, wait: number, fn: () => Promise<Response>): Promise<Response> => {
+    return new Promise((resolve, reject) => {
+        fn()
+            .then(resolve)
+            .catch((error: Error) => {
+                if (tries === 1) {
+                    reject(error);
+                    return;
+                }
+                setTimeout(() => {
+                    retry(tries - 1, wait, fn).then(resolve, reject);
+                }, wait);
+            });
+    });
+};
+
 export default async (request: IRequest, env: Env, ctx: ExecutionContext) => {
     const url = new URL(request.url);
     const cache = caches.default;
     let response = await cache.match(request as any as Request);
     if (!response) {
-        response = await fetch(`${env.B2URL}/file/${env.B2BUCKET}/${env.PATH_PREFIX}${url.pathname}`);
-        if (response.status === 404) {
-            return null;
+        response = await retry(3, 500, async () => {
+            const res = await fetch(`${env.B2URL}/file/${env.B2BUCKET}/${env.PATH_PREFIX}${url.pathname}`);
+            if (res.status === 404) {
+                throw new Error("404");
+            }
+            return res;
+        });
+
+        if (!response || response.status === 404) {
+            throw new Error("404");
         }
+
         response = new Response(response.body, { ...response, headers: formatHeaders(response.headers, 86400) });
         ctx.waitUntil(cache.put(request as any as Request, response.clone()));
     }
