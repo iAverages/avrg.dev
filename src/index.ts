6ada7b8915606e5d07c019adc1025c6fdeec9a29
iAverages <me@danielraybone.com>
2025-07-13 15:06:14 +0100

    add uploading

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
+import { BackblazeB2 } from "cloudflare-b2/src";
 import { Hono } from "hono";
+import { basicAuth } from "hono/basic-auth";
 import { formatHeaders, retry } from "./utils";
 
 const app = new Hono<{ Bindings: CloudflareBindings }>();
+
+let b2: BackblazeB2 | null = null;
 
 app.get("/:url", async (c) => {
 	const url = c.req.param("url");
@@ -55,5 +59,44 @@ app.get("/:url", async (c) => {
 	}
 	return response;
 });
+
+app
+	// need to access context for env variables
+	.use("*", async (c, next) =>
+		basicAuth({ username: c.env.AUTH_USER, password: c.env.AUTH_PASS })(
+			c,
+			next,
+		),
+	)
+	.post("/api/upload", async (c) => {
+		if (!b2) {
+			// i do not think i can get env without accessing env that is
+			// passed into the function, this is just a slight work around
+			b2 = new BackblazeB2({
+				applicationKeyId: c.env.B2_KEY_ID,
+				applicationKey: c.env.B2_KEY,
+			});
+			await b2.authorizeAccount();
+		}
+		await b2.getUploadUrl(c.env.B2_BUCKET_ID);
+		const formData = await c.req.formData();
+		const file = formData.get("file");
+		if (!file || typeof file === "string") {
+			return c.json({ message: "no file uploaded" }, { status: 400 });
+		}
+
+		// i do this here instead of waiting for the upload to complete
+		// so i get the link faster and can post it places sooner
+		c.executionCtx.waitUntil(
+			b2.uploadFile(file, `${c.env.PATH_PREFIX}/${file.name}`).catch((e) => {
+				console.error("upload failed:", e);
+			}),
+		);
+
+		const url = new URL(c.req.url);
+		const domain = url.hostname;
+		const proto = url.protocol;
+		return c.json({ link: `${proto}//${domain}/${file.name}` });
+	});
 
 export default app;
